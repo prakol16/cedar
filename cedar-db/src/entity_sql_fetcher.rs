08462a6be95ee6931628405124753026b3bc593c
Praneeth Kolichala <kolichp@amazon.com>
2023-07-20 16:54:15 -0400

    Add sql query er

--- cedar-db/src/entity_sql_fetcher.rs
+++ cedar-db/src/entity_sql_fetcher.rs
@@ -1,18 +1,37 @@
 use std::{collections::{HashMap, HashSet}, borrow::Cow};
 
 use cedar_policy::{EntityTypeName, PartialValue, EntityUid, EntityDatabase, ParsedEntity, Mode};
-use rusqlite::{Connection, Row};
+use ref_cast::RefCast;
+use rusqlite::{Connection, Row, OptionalExtension, types::{FromSql, ValueRef}};
 
 
 pub struct EntitySQLFetchTable {
     conn: Connection,
-    table: HashMap<EntityTypeName, EntitySQLTypeFetcher>
+    table: &'static HashMap<EntityTypeName, EntitySQLTypeFetcher>
 }
 
 pub struct EntitySQLTypeFetcher {
     table_name: &'static str,
     attr_names: Vec<&'static str>,
-    converter: Box<dyn EntitySQLAttrConverter>,
+    converter: Box<dyn EntitySQLAttrConverter + Send + Sync>,
+}
+
+impl EntitySQLTypeFetcher {
+    pub fn new(table_name: &'static str, attr_names: Vec<&'static str>, converter:  Box<dyn EntitySQLAttrConverter + Send + Sync>) -> Self {
+        Self {
+            table_name,
+            attr_names,
+            converter
+        }
+    }
+
+    pub fn new_default(table_name: &'static str, attr_names: Vec<&'static str>) -> Self {
+        Self {
+            table_name,
+            attr_names: attr_names.clone(),
+            converter: Box::new(EntitySQLAttrConverterImpl::of_names(attr_names))
+        }
+    }
 }
 
 pub trait EntitySQLAttrConverter {
@@ -20,20 +39,82 @@ pub trait EntitySQLAttrConverter {
     fn convert(&self, query_result: &Row) -> Result<HashMap<String, PartialValue>, rusqlite::Error>;
 }
 
+impl EntitySQLFetchTable {
+    pub fn new(conn: Connection, table: &'static HashMap<EntityTypeName, EntitySQLTypeFetcher>) -> Self {
+        Self {
+            conn,
+            table
+        }
+    }
+}
+
+pub struct EntitySQLAttrConverterImpl {
+    // TODO: allow type checking?
+    attr_names: Vec<(usize, &'static str)>,
+}
+
+#[derive(Debug, Clone, PartialEq, RefCast)]
+#[repr(transparent)]
+struct SQLPartialValue(Option<PartialValue>);
+
+impl FromSql for SQLPartialValue {
+    fn column_result(value: ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        match value {
+            ValueRef::Null => Ok(SQLPartialValue(None)),
+            ValueRef::Integer(x) => Ok(SQLPartialValue(Some(x.into()))),
+            // TODO: use decimal type
+            ValueRef::Real(_) => Err(rusqlite::types::FromSqlError::InvalidType),
+            ValueRef::Text(s) => {
+                let decoded = std::str::from_utf8(s).map_err(|_| rusqlite::types::FromSqlError::InvalidType)?;
+                Ok(SQLPartialValue(Some(decoded.into())))
+            },
+            ValueRef::Blob(_) => Err(rusqlite::types::FromSqlError::InvalidType),
+        }
+    }
+}
+
+impl EntitySQLAttrConverterImpl {
+    pub fn new(attr_names: Vec<(usize, &'static str)>) -> Self {
+        Self {
+            attr_names
+        }
+    }
+
+    pub fn of_names(attr_names: Vec<&'static str>) -> Self {
+        Self {
+            attr_names: attr_names.into_iter().enumerate().map(|(i, n)| (i, n)).collect()
+        }
+    }
+}
+
+impl EntitySQLAttrConverter for EntitySQLAttrConverterImpl {
+    fn convert(&self, query_result: &Row) -> Result<HashMap<String, PartialValue>, rusqlite::Error> {
+        self.attr_names.iter()
+            .filter_map(|(ind, nm)| {
+                match query_result.get::<_, SQLPartialValue>(*ind) {
+                    Ok(SQLPartialValue(Some(v))) => Some(Ok((nm.to_string(), v))),
+                    Ok(SQLPartialValue(None)) => None,
+                    Err(e) => Some(Err(e)),
+                }
+            })
+            .collect()
+    }
+}
 
 impl EntityDatabase for EntitySQLFetchTable {
     fn get<'e>(&'e self, uid: &EntityUid) -> Option<Cow<'e, ParsedEntity>> {
         let type_fetcher = self.table.get(&uid.type_name())?;
         // TODO: escape attr names and table_name?
-        let query = format!("SELECT \"{}\" FROM \"{}\" WHERE uid = ?",
+        let query = format!("SELECT {} FROM \"{}\" WHERE uid = ?",
             type_fetcher.attr_names
             .iter()
             .map(|x| format!("\"{}\"", x))
             .collect::<Vec<String>>()
             .join(", "), type_fetcher.table_name);
-        let result: Result<HashMap<String, PartialValue>, _> = self.conn.query_row(&query,
-            &[uid.id().as_ref()], |row| type_fetcher.converter.convert(row));
-        result.ok().map(|attrs| Cow::Owned(ParsedEntity::new(uid.clone(), attrs, HashSet::new())))
+        let result: Option<HashMap<String, PartialValue>> = self.conn.query_row(&query,
+            &[uid.id().as_ref()], |row| type_fetcher.converter.convert(row))
+            .optional().expect("SQL query failed");  // TODO: if panic, return a result (maybe make return type Result<Option<...>>?)
+        result.map(|attrs| Cow::Owned(ParsedEntity::new(uid.clone(), attrs, HashSet::new())))
     }
 
     fn partial_mode(&self) -> Mode {
--- cedar-db/src/main.rs
+++ cedar-db/src/main.rs
@@ -1,38 +1,43 @@
+use std::{collections::HashMap, str::FromStr};
+
+use cedar_policy::{EntityTypeName, EntityDatabase, EntityUid, Authorizer, Request, Context};
+use entity_sql_fetcher::EntitySQLFetchTable;
+use rusqlite::Connection;
+use lazy_static::lazy_static;
+
+use crate::entity_sql_fetcher::*;
 
 mod entity_sql_fetcher;
 
-use std::borrow::Cow;
+lazy_static! {
 
-use cedar_policy::{EntityDatabase, EntityUid, ParsedEntity, Mode};
-use rusqlite::{Connection, Result};
+    static ref ENTITY_TABLE: HashMap<EntityTypeName, EntitySQLTypeFetcher> = {
+        // let users_entity_parser: EntitySQLAttrConverterImpl = {
+        //     EntitySQLAttrConverterImpl::new(vec![
+        //         (0, "name"),
+        //         (1, "age")
+        //     ])
+        // };
 
-
-struct SQLiteDB {
-    conn: Connection,
-}
-
-impl EntityDatabase for SQLiteDB {
-    fn get<'e>(&'e self, uid: &EntityUid) -> Option<Cow<'e, ParsedEntity>> {
-        todo!()
-    }
-
-    fn partial_mode(&self) -> Mode {
-        todo!()
-    }
+        let mut table = HashMap::new();
+        // table.insert(EntityTypeName::from_str("Users").unwrap(), EntitySQLTypeFetcher::new("users", vec!["name", "age"], Box::new(users_entity_parser)));
+        table.insert(EntityTypeName::from_str("Users").unwrap(), EntitySQLTypeFetcher::new_default("users", vec!["name", "age"]));
+        table
+    };
 }
 
 fn main() {
-    println!("Hello, world!");
-    create_example_database().unwrap();
-}
+    let conn = Connection::open("cedar-db/example.db").expect("Connection failed");
+    let table: EntitySQLFetchTable = EntitySQLFetchTable::new(conn, &ENTITY_TABLE);
+    let result = table.get(&"Users::\"0\"".parse().unwrap());
+    println!("{:?}", result);
 
-fn create_example_database() -> Result<()> {
-    let conn = Connection::open("cedar-db-example/example.db")?;
-    conn.query_row("SELECT name, age FROM users", [], |row| {
-        let name: String = row.get(0)?;
-        let age: i32 = row.get(1)?;
-        println!("found person: {} {}", name, age);
-        Ok(())
-    })?;
-    Ok(())
+    // let auth = Authorizer::new();
+    // let result = auth.is_authorized_parsed(
+    //     &Request::new(Some("Users::\"0\"".parse().unwrap()),
+    //         Some("Actions::\"view\"".parse().unwrap()),
+    //         Some("Photos::\"20\"".parse().unwrap()), Context::empty())
+    //     , &"permit(principal, action, resource) when { principal.age == 20 };".parse().unwrap(),
+    //     &table);
+    // println!("Result {}", result);
 }
